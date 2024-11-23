39345643ecc9a3c7494931850c2d55845b798e4a
RomanRomanyuk <romaniuk.roman1121@vu.cdu.edu.ua>
2024-11-23 19:46:30 +0200

    Add Retrofit2(instance and interface)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,6 @@ dependencies {
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 }
